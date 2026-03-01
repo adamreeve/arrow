295df5fe38dfbcd5afddbc1309fdd8dc67c9d2a3
Adam Reeve <adreeve@gmail.com>
2026-03-02 11:14:04 +1300

    Add tests for aes256 data

--- cpp/src/parquet/encryption/read_configurations_test.cc
+++ cpp/src/parquet/encryption/read_configurations_test.cc
@@ -292,4 +292,60 @@ INSTANTIATE_TEST_SUITE_P(
             5, "encrypt_columns_and_footer_disable_aad_storage.parquet.encrypted"),
         std::make_tuple(6, "encrypt_columns_and_footer_ctr.parquet.encrypted")));
 
+TEST(DecryptionTestsAes256, UniformEncryption) {
+  FileDecryptor decryptor;
+
+  std::string file_path = data_file("aes256/uniform_encryption.parquet.encrypted");
+
+  SecureString footer_key("01234567890123456789012345678901");
+
+  parquet::FileDecryptionProperties::Builder builder;
+  auto decryption_properties = builder.footer_key(footer_key)->build();
+
+  decryptor.DecryptFile(file_path, decryption_properties);
+}
+
+TEST(DecryptionTestsAes256, PlaintextFooterWithColumnEnc) {
+  FileDecryptor decryptor;
+
+  std::vector<std::string> file_paths {
+    data_file("aes256/encrypt_columns_and_footer.parquet.encrypted"),
+    data_file("aes256/encrypt_columns_plaintext_footer.parquet.encrypted"),
+    data_file("aes256/encrypt_columns_and_footer_disable_aad_storage.parquet.encrypted"),
+    data_file("aes256/encrypt_columns_and_footer_ctr.parquet.encrypted"),
+  };
+
+  for (const auto& file_path : file_paths) {
+    SecureString footer_key("01234567890123456789012345678901");
+    ColumnPathToDecryptionPropertiesMap column_keys;
+    ColumnDecryptionProperties::Builder builder_1("double_field");
+    ColumnDecryptionProperties::Builder builder_2("float_field");
+    ColumnDecryptionProperties::Builder builder_3("boolean_field");
+    ColumnDecryptionProperties::Builder builder_4("int32_field");
+    ColumnDecryptionProperties::Builder builder_5("ba_field");
+    ColumnDecryptionProperties::Builder builder_6("flba_field");
+    ColumnDecryptionProperties::Builder builder_7("int64_list_field");
+    ColumnDecryptionProperties::Builder builder_8("int64_field");
+    column_keys["double_field"] = builder_1.key(SecureString("12345678901234567890123456789012"))->build();
+    column_keys["float_field"] = builder_2.key(SecureString("12345678901234567890123456789013"))->build();
+    column_keys["boolean_field"] = builder_3.key(SecureString("12345678901234567890123456789014"))->build();
+    column_keys["int32_field"] = builder_4.key(SecureString("12345678901234567890123456789015"))->build();
+    column_keys["ba_field"] = builder_5.key(SecureString("12345678901234567890123456789016"))->build();
+    column_keys["flba_field"] = builder_6.key(SecureString("12345678901234567890123456789017"))->build();
+    column_keys["int64_list_field"] = builder_7.key(SecureString("12345678901234567890123456789018"))->build();
+    column_keys["int64_field"] = builder_8.key(SecureString("12345678901234567890123456789019"))->build();
+
+    parquet::FileDecryptionProperties::Builder builder;
+    builder.footer_key(footer_key)->column_keys(column_keys);
+
+    if (file_path.find("disable_aad_storage") != std::string::npos) {
+      builder.aad_prefix("tester");
+    }
+
+    auto decryption_properties = builder.build();
+
+    decryptor.DecryptFile(file_path, decryption_properties);
+  }
+}
+
 }  // namespace parquet::encryption::test
